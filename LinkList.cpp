9e8da1cf8ac71663d9a645045c0aea58448b5eb3
godisboom <guxukai365@gmail.com>
2021-03-04 23:37:18 +0800

    done

--- LinkList.cpp
+++ LinkList.cpp
@@ -8,6 +8,46 @@ typedef struct LNode
 }LNode,*LinkList;
 
 bool InitLinkList(LinkList &L){
-    L=NULL;
+    L=(LNode*)malloc(sizeof(LNode));
+    if (L==NULL)
+    {
+        return false;
+    }
+    L->next=NULL;
     return true;
+}
+
+bool Empty(LinkList &L){
+    return (L->next == NULL);
+}
+
+LinkList List_TailInsert(LinkList &L){
+    int x;
+    L=(LNode*)malloc(sizeof(LNode));
+    LNode *s,*r=L;
+    scanf("%d",&x);
+    while (x!=10086)
+    {
+        s=(LNode*)malloc(sizeof(LNode));
+        s->data=x;
+        r->next=s;
+        r=s;
+        scanf("%d",&x);
+    }
+    r->next=NULL;
+    return L;
+}
+
+int main(){
+    LinkList L;
+    InitLinkList(L);
+    List_TailInsert(L);
+    LNode *p=L;
+    while (p->next!=NULL)
+    {
+        p=p->next;
+        printf("%d \n",p->data);
+    }
+    system("pause");
+    
 }
